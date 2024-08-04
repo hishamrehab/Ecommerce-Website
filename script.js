e5e01b51afb7d2c0dbf3737e4134dd682254bdae
My Name <hishamrehab309@gmail.com>
2024-08-04 17:31:31 +0300

    products section functionality

--- script.js
+++ script.js
@@ -79,9 +79,33 @@ const products = [
   },
 ];
 
+let chosenProduct = products[0];
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
+    // change the current slide
+
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    //change the chosen product
+    chosenProduct = products[index];
+    // change texts of currentProduct
+    currentProductTitle.textContent = chosenProduct.title;
+    currentProductPrice.textContent = "$" + chosenProduct.price;
+    currentProductImg.src = chosenProduct.colors[0].img;
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = chosenProduct.colors[index].code;
+    });
   });
 });
-// products section
+
+// change image based on colors
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = chosenProduct.colors[index].img;
+  });
+});
